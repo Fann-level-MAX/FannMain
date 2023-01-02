49f50c67bbb9d5875b5167756414bdac127d736a
Fgtps <rilisan5@gmail.com>
2023-01-02 08:53:15 +0700

    FannMain

---
+++ README.md
@@ -0,0 +1,54 @@
+<p align="center">
+    <img src="https://telegra.ph/file/a8641df0da430cb23f976.jpg" width="100%" style="margin-left: auto;margin-right: auto;display: block;">
+</p>
+<h1 align="center">Yushino V2</h1>
+
+---------
+
+## ```Heroku Buildpack```
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Fgtps/FannBotz)
+
+| BuildPack | LINK |
+|--------|--------|
+| **FFMPEG** |[here](https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest) |
+| **IMAGEMAGICK** | [here](https://github.com/DuckyTeam/heroku-buildpack-imagemagick) |
+
+## TERMUX USER
+```bash
+$ pkg upgrade && pkg update
+$ pkg install git -y
+$ pkg install nodejs -y
+$ pkg install ffmpeg -y
+$ pkg install imagemagick -y
+$ git clone https://github.com/Hyuura-Official/YushinoMdV2
+$ cd YushinoMdV2
+$ npm i 
+```
+If error try using yarn instead of npm, see [here](https://github.com/Jikarinka/Kannabotv2#if-npm-install-failed--try--using-yarn-instead-of-npm)
+```bash
+$ node .
+```
+
+#### If npm install failed, try using yarn instead of npm
+```bash
+$ pkg install yarn -y
+$ yarn install
+```
+---------
+
+## Run 🏃
+
+```bash
+node .
+```
+
+---------
+
+#
+### 📮 S&K
+1. Jangan diperjual belikan Script ini
+2. Sebelum pakai jangan lupa kasih star
+3. Follow Github !
+4. Jangan salah gunakan script ini!
+
+---------
---
+++ plugins/youtube-ytv.js
@@ -0,0 +1,49 @@
+let limit = 80
+import fetch from 'node-fetch'
+import { youtubeSearch, youtubedl, youtubedlv2, youtubedlv3 } from '@bochilteam/scraper';
+let { MessageType } = (await import('@adiwajshing/baileys')).default
+
+let handler = async (m, { conn, args, isPrems, isOwner }) => {
+  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+  let chat = global.db.data.chats[m.chat]
+  const isY = /y(es)/gi.test(args[1])
+  const { thumbnail, video: _video, title} = await youtubedl(args[0]).catch(async _ => await youtubedlv2(args[0])).catch(async _ => await youtubedlv3(args[0]))
+  const limitedSize = (isPrems || isOwner ? 99 : limit) * 1024
+  let video, source, res, link, lastError, isLimit
+  for (let i in _video) {
+    try {
+      video = _video[i]
+      isLimit = limitedSize < video.fileSize
+      if (isLimit) continue
+      link = await video.download()
+      if (link) res = await fetch(link)
+      isLimit = res?.headers.get('content-length') && parseInt(res.headers.get('content-length')) < limitedSize
+      if (isLimit) continue
+      if (res) source = await res.arrayBuffer()
+      if (source instanceof ArrayBuffer) break
+    } catch (e) {
+      video = source = link = null
+      lastError = e
+    }
+  }
+  if ((!(source instanceof ArrayBuffer) || !link || !res.ok) && !isLimit) throw 'Error: ' + (lastError || 'Can\'t download video')
+  if (!isY && !isLimit) await conn.sendMessage(m.chat, { react:{ text:"🎥", key:m.key }})
+  let _thumb = {}
+  try { _thumb = { thumbnail: await (await fetch(thumbnail)).buffer() } }
+  catch (e) { }
+  if (!isLimit) await conn.sendFile(m.chat, link, title + '.mp4', `.`.trim(), m, false, {
+    ..._thumb,
+    asDocument: chat.useDocument
+  })
+  setTimeout(function(){
+  conn.sendMessage(m.chat, { react:{ text:"", key:m.key }})
+}, 5000)
+}
+handler.help = ['mp4', 'v', ''].map(v => 'yt' + v + ` <url> <without message>`)
+handler.tags = ['downloader']
+handler.command = /^yt(v|mp4)?$/i
+
+handler.owner = true
+
+
+export default handler
